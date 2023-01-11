d789dfc6981821cd9b677e03d1cc21955d101af4
Kritlimtree <krit.li@ku.th>
2023-01-12 06:53:05 +0700

    Update indexLoginIsTrue.php

--- backend/indexLoginIsTrue.php
+++ backend/indexLoginIsTrue.php
@@ -10,6 +10,8 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+    	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
 		<?php
 		@ini_set('display_errors', '0');
 		session_start(); 
@@ -25,16 +27,156 @@
 			$_SESSION["tp"]=$_POST['tp'];
 		}
 		?>
+		<style>
+		body, input, select, textarea {
+  color: #7f888f;
+  font-family: "Open Sans", sans-serif;
+  font-size: 13pt;
+  font-weight: 400;
+  line-height: 1.65; }
+  @media screen and (max-width: 1680px) {
+    body, input, select, textarea {
+      font-size: 11pt; } }
+  @media screen and (max-width: 1280px) {
+    body, input, select, textarea {
+      font-size: 10pt; } }
+  @media screen and (max-width: 360px) {
+    body, input, select, textarea {
+      font-size: 9pt; } }
+
+a {
+  -moz-transition: color 0.2s ease-in-out, border-bottom-color 0.2s ease-in-out;
+  -webkit-transition: color 0.2s ease-in-out, border-bottom-color 0.2s ease-in-out;
+  -ms-transition: color 0.2s ease-in-out, border-bottom-color 0.2s ease-in-out;
+  transition: color 0.2s ease-in-out, border-bottom-color 0.2s ease-in-out;
+  border-bottom: dotted 1px;
+  color: #2c91f0;
+  text-decoration: none; }
+  a:hover {
+    border-bottom-color: #2c91f0;
+    color: #2c91f0 !important; }
+    a:hover strong {
+      color: inherit; }
+
+strong, b {
+  color: #3d4449;
+  font-weight: 600; }
+
+em, i {
+  font-style: italic; }
+
+p {
+  margin: 0 0 2em 0; }
+
+h1, h2, h3, h4, h5, h6 {
+  color: #3d4449;
+  font-family: "Roboto Slab", serif;
+  font-weight: 700;
+  line-height: 1.5;
+  margin: 0 0 1em 0; }
+  h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {
+    color: inherit;
+    text-decoration: none;
+    border-bottom: 0; }
+
+h1 {
+  font-size: 4em;
+  margin: 0 0 0.5em 0;
+  line-height: 1.3; }
+
+h2 {
+  font-size: 1.75em; }
+
+h3 {
+  font-size: 1.25em; }
+
+h4 {
+  font-size: 1.1em; }
+
+h5 {
+  font-size: 0.9em; }
+
+h6 {
+  font-size: 0.7em; }
+
+@media screen and (max-width: 1680px) {
+  h1 {
+    font-size: 3.5em; } }
+
+@media screen and (max-width: 980px) {
+  h1 {
+    font-size: 3.25em; } }
+
+@media screen and (max-width: 736px) {
+  h1 {
+    font-size: 2em;
+    line-height: 1.4; }
+  h2 {
+    font-size: 1.5em; } }
+
+sub {
+  font-size: 0.8em;
+  position: relative;
+  top: 0.5em; }
+
+sup {
+  font-size: 0.8em;
+  position: relative;
+  top: -0.5em; }
+
+blockquote {
+  border-left: solid 3px rgba(210, 215, 217, 0.75);
+  font-style: italic;
+  margin: 0 0 2em 0;
+  padding: 0.5em 0 0.5em 2em; }
+
+code {
+  background: rgba(230, 235, 237, 0.25);
+  border-radius: 0.375em;
+  border: solid 1px rgba(210, 215, 217, 0.75);
+  font-family: "Courier New", monospace;
+  font-size: 0.9em;
+  margin: 0 0.25em;
+  padding: 0.25em 0.65em; }
+
+pre {
+  -webkit-overflow-scrolling: touch;
+  font-family: "Courier New", monospace;
+  font-size: 0.9em;
+  margin: 0 0 2em 0; }
+  pre code {
+    display: block;
+    line-height: 1.75;
+    padding: 1em 1.5em;
+    overflow-x: auto; }
+
+hr {
+  border: 0;
+  border-bottom: solid 1px rgba(210, 215, 217, 0.75);
+  margin: 2em 0; }
+  hr.major {
+    margin: 3em 0; }
+
+.align-left {
+  text-align: left; }
+
+.align-center {
+  text-align: center; }
+
+.align-right {
+  text-align: right; }
+.display-inline {
+    display: inline;
+}
+		</style>
 	</head>
 	<body class="is-preload">
 
 		<!-- Wrapper -->
 			<div id="wrapper">
-
 				<!-- Main -->
 					<div id="main">
 						<div class="inner">
-
 							<!-- Header -->
 								<header id="header">
 									<p>ยินดีต้อนรับ คุณ admin</p>
@@ -53,24 +195,26 @@
 									</ul>
 									<!-- <i class="fa fa-user-circle" aria-hidden="true"></i> -->
 								</header>
-
 							<!-- Banner -->
 								<section id="banner">
 									<header>
 									</header>
 									<div class="Center">
-										<h3>ออร์เดอร์ที่อยู่ในระหว่างการดำเนินการ</h3>
 										
+									<input type="text" id="Search_All">
+										<h3 class="display-inline">ออร์เดอร์ที่อยู่ในระหว่างการดำเนินการ</h3><nobr>
+
 										<div class="table-wrapper">
 											<table class="alt">
+											<table class="alt" id="table1">
 												<thead>
 													<tr>
-														<th>ชื่อ</th>
-														<th>นามสกุล</th>
-														<th>วันที่สั่งออร์เดอร์</th>
-														<th>id order</th>
-														<th>อยู่ในระหว่างการดำเนินการ(วัน)</th>
-														<th>สถานะ</th>
+														<td>ชื่อ</td>
+														<td>นามสกุล</td>
+														<td>วันที่สั่งออร์เดอร์</td>
+														<td>id order</td>
+														<td>อยู่ในระหว่างการดำเนินการ(วัน)</td>
+														<td>สถานะ</td>
 														
 													</tr>
 												</thead>
@@ -79,7 +223,7 @@
 														<td>นายนวพร</td>
 														<td>หลงเหลือยิ่ง</td>
 														<td>13/11/65</td>
-														<td><a href="test1.php" >bts1111220001</a></td>
+														<td><a href="testtest.php" >bts1111220001</a></td>
 														<td>2</td>
 														<td>กำลังประเมินราคา</td>
 														
@@ -88,7 +232,7 @@
 														<td>นางนงนวล</td>
 														<td>อุดรรัตนธี</td>
 														<td>12/11/65</td>
-														<td><a href="testtest.html" >bts1112220001</a></td>
+														<td><a href="testtest.php" >bts1112220001</a></td>
 														<td>3</td>
 														<td>กำลังประเมินราคา</td>
 														
@@ -97,7 +241,7 @@
 														<td>บุษกร</td>
 														<td>นภาลัย</td>
 														<td>11/11/65</td>
-														<td><a href="test1.php" >bts1113220001</a></td>
+														<td><a href="testtest.php" >bts1113220001</a></td>
 														<td>4</td>
 														<td>กำลังแก้ไขตัวอย่าง</td>
 														
@@ -126,12 +270,13 @@
 									<h3>ออเดอร์ที่ยังไม่ได้ประเมินราคา</h3>
 									<div class="table-wrapper">
 										<table class="alt">
+										<table class="alt" id="table2">
 											<thead>
 												<tr>		
-													<th>ชื่อ</th>
-													<th>นามสกุล</th>
-													<th>วันที่สั่งออร์เดอร์</th>
-													<th>id order</th>
+													<td>ชื่อ</td>
+													<td>นามสกุล</td>
+													<td>วันที่สั่งออร์เดอร์</td>
+													<td>id order</td>
 													
 												</tr>
 											</thead>
@@ -169,12 +314,13 @@
 									<h3>ออเดอร์ที่ต้องแก้ไขตัวอย่าง</h3>
 									<div class="table-wrapper">
 										<table class="alt">
+										<table class="alt" id="table3">
 											<thead>
 												<tr>												
-													<th>id order</th>
-													<th>วันที่ลูกค้าให้แก้ไข</th>
-													<th>รายละเอียดการแก้ไข</th>
-													
+													<td>id order</td>
+													<td>วันที่ลูกค้าให้แก้ไข</td>
+													<td>รายละเอียดการแก้ไข</td>
+
 													
 												</tr>
 											</thead>
@@ -239,7 +385,6 @@
 										</article>
 									</div>
 								</section> -->
-
 							<!-- Section -->
 								<!-- <section>
 									<header class="major">
@@ -296,21 +441,17 @@
 										</article>
 									</div>
 								</section> -->
-
 						</div>
 					</div>
-
 				<!-- Sidebar -->
 					<div id="sidebar">
 						<div class="inner">
-
 							<!-- Search -->
 								<!-- <section id="search" class="alt">
 									<form method="post" action="#">
 										<input type="text" name="query" id="query" placeholder="Search" />
 									</form>
 								</section> -->
-
 							<!-- Menu -->
 								<nav id="menu">
 									<header class="major">
@@ -346,7 +487,6 @@
 										<!-- <li><a href="contact.html">ติดต่อเรา</a></li> -->
 									</ul>
 								</nav>
-
 							<!-- Section -->
 								<!-- <section>
 									<header class="major">
@@ -371,18 +511,67 @@
 									</ul>
 								</section> -->
 								
-
 						</div>
 					</div>
-
 			</div>
-
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/browser.min.js"></script>
 			<script src="assets/js/breakpoints.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<script src="assets/js/main.js"></script>
+			<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+			<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+			<script src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap5.min.js"></script>
+
+			<script>
+
+	$.fn.dataTableExt.oApi.fnFilterAll = function (oSettings, sInput, iColumn, bRegex, bSmart) {
+               var settings = $.fn.dataTableSettings;
+
+               for (var i = 0; i < settings.length; i++) {
+                   settings[i].oInstance.fnFilter(sInput, iColumn, bRegex, bSmart);
+               }
+           };
+
+           $(document).ready(function () {
+               $('#table1').dataTable({
+                   "bPaginate": true,
+				   "dom": '<"top"l>rt<"bottom"p>'
+               });
+               var oTable0 = $("#table1").dataTable();
+
+               $("#Search_All").keyup(function () {
+                   // Filter on the column (the index) of this element
+                   oTable0.fnFilterAll(this.value);
+               });
+           });
+
+           $(document).ready(function () {
+               $('#table2').dataTable({
+                   "bPaginate": true,
+				   "dom": '<"top"l>rt<"bottom"p>'
+               });
+               var oTable1 = $("#table1").dataTable();
+
+               $("#Search_All").keyup(function () {
+                   // Filter on the column (the index) of this element
+                   oTable1.fnFilterAll(this.value);
+               });
+           });
+		   $(document).ready(function () {
+               $('#table3').dataTable({
+                   "bPaginate": true,
+				   "dom": '<"top"l>rt<"bottom"p>'
+               });
+               var oTable1 = $("#table1").dataTable();
+
+               $("#Search_All").keyup(function () {
+                   // Filter on the column (the index) of this element
+                   oTable1.fnFilterAll(this.value);
+               });
+           });
+    </script>
 
 	</body>
 </html>
